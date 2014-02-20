f169d0f2507499d12bdfd9f146cf8956b11e22ed
Jakob Ståhl <jakob.stahl91@gmail.com>
2014-02-20 11:06:42 -0800

    Added salt and database config

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'quQeta3asUrUn8quve8resPePrUqeswe');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '78287288585854837836442253888446');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
