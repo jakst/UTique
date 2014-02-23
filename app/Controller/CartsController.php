76f1975a2d20645fded083695a3687efa313f036
sofienilsson90 <sofienilsson__@hotmail.com>
2014-02-23 09:07:19 +0100

    Made it possible to save several items

--- app/Controller/CartsController.php
+++ app/Controller/CartsController.php
@@ -2,8 +2,8 @@
 class CartsController extends AppController {
 
 	public function add_to_cart($id){
-		$this->Session->write('User.item', $id);
-		$this->redirect(array('controller' => 'tees', 'action' => 'view', $id);
+		$this->Session->write('Cart.'.$id, 1);
+		$this->redirect(array('controller' => 'tees', 'action' => 'view', $id));
 	}
 }
 ?>
