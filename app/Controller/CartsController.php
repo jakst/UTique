10121fcf21f4cd29db74c3c5e39a21b83d4f5488
sofienilsson90 <sofienilsson__@hotmail.com>
2014-02-23 09:28:52 +0100

    error check + ability to add more than 1 of each tee

--- app/Controller/CartsController.php
+++ app/Controller/CartsController.php
@@ -1,8 +1,25 @@
 <?php
 class CartsController extends AppController {
 
-	public function add_to_cart($id){
-		$this->Session->write('Cart.'.$id, 1);
+	public function add_to_cart($id = null){
+		if (!$id) {
+            throw new NotFoundException(__('Hej'));
+        }
+		
+		$tee = $this->Tee->findByProductid($id);
+        
+		if (!$tee) {
+            throw new NotFoundException(__('Finns ingen sådan t-shirt'));
+        }
+		unset($tee);
+	
+		if ($this->Session->check('Cart'.$id)) {
+			$amount = $this->Session->read('Cart.'.$id);
+			$amount++;
+			$this->Session->write('Cart.'.$id, $amount);
+		} else{
+			$this->Session->write('Cart.'.$id, 1);
+		}
 		$this->redirect(array('controller' => 'tees', 'action' => 'view', $id));
 	}
 }
