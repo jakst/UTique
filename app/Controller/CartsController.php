476700f9726688303922a2a60f136af03fe5af89
Jakob Ståhl <jakob.stahl91@gmail.com>
2014-02-26 13:29:51 -0800

    Added size check in add_to_cart

--- app/Controller/CartsController.php
+++ app/Controller/CartsController.php
@@ -2,16 +2,17 @@
 class CartsController extends AppController {
 
 	public function add_to_cart($id = null, $size = null){
-		if (!$id) {
-            throw new NotFoundException(__('Hej'));
+		if (!$id || !$size) {
+            throw new NotFoundException(__('Kunde inte lägga till varan i varukorgen'));
 		}
 	
 		if ($this->Session->check('Cart.'.$id.$size)) {
 			$amount = $this->Session->read('Cart.'.$id.$size);
 			$this->Session->write('Cart.'.$id.$size, $amount+1);
-		} else{
+		} else {
 			$this->Session->write('Cart.'.$id.$size, 1);
 		}
+		
 		$this->redirect(array('controller' => 'tees', 'action' => 'view', $id));
 	}
 	
@@ -25,8 +26,7 @@ class CartsController extends AppController {
 		$price = 99;
 		$amount = 3;
 		$size = 'S';
-			
-			
+		
 		$tees = array(
 			 'Tee' => array(
 				'id' => $id,
