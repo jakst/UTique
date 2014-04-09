a96c2aaffa17d581fff29af581f688733c45e4f8
Ylva Johansson <ylvjo680@student.liu.se>
2014-04-09 15:40:14 +0800

    new pic

--- app/Controller/InventoryItemsController.php
+++ app/Controller/InventoryItemsController.php
@@ -1,21 +1,21 @@
 ﻿<?php
 class InventoryItemsController extends AppController {
 	public function check_inventory(){
-			$cart = $this->Session->read('Cart');
-			$data['InventoryItem'] = Hash::combine($this->InventoryItem->find('all'), '{n}.InventoryItem.id', '{n}.InventoryItem');
-			foreach ($cart as $id => $tee):
-				foreach ($tee['sizes'] as $size => $item):
-					$item_id = $item['item_id'];
-					if(($data['InventoryItem'][$item_id]['amount'] - $item['amount']) < 0){
-						$errorMessage = "Tyvärr finns det bara ".$data['InventoryItem'][$item_id]['amount']." kvar i lager av t-shirt ".$tee['Tee']['name']." i storlek ".$size.". Var god ändra din beställning.";
-						$this->Session->setFlash($errorMessage, 'flash/error');
-						$this->redirect(array('controller' => 'carts', 'action' => 'view'));
-						//$this->redirect(array('controller' => 'carts', 'action' => 'view', 'errorMessage' => $errorMessage));
-					}
-				endforeach;
+		$cart = $this->Session->read('Cart');
+		$data['InventoryItem'] = Hash::combine($this->InventoryItem->find('all'), '{n}.InventoryItem.id', '{n}.InventoryItem');
+		foreach ($cart as $id => $tee):
+			foreach ($tee['sizes'] as $size => $item):
+				$item_id = $item['item_id'];
+				if(($data['InventoryItem'][$item_id]['amount'] - $item['amount']) < 0){
+					$errorMessage = "Tyvärr finns det bara ".$data['InventoryItem'][$item_id]['amount']." kvar i lager av t-shirt ".$tee['Tee']['name']." i storlek ".$size.". Var god ändra din beställning.";
+					$this->Session->setFlash($errorMessage, 'flash/error');
+					$this->redirect(array('controller' => 'carts', 'action' => 'view'));
+					//$this->redirect(array('controller' => 'carts', 'action' => 'view', 'errorMessage' => $errorMessage));
+				}
 			endforeach;
-			
-			$this->redirect(array('controller' => 'orders', 'action' => 'create_order'));
+		endforeach;
+		
+		$this->redirect(array('controller' => 'orders', 'action' => 'create_order'));
 	}
 }
 ?>
