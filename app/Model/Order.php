599f9c84c52f4423f43016b1ba7dec544957728e
Jakob Ståhl <jakob.stahl91@gmail.com>
2014-04-16 13:28:09 +0800

    Invetory now updates correctly and fatal error messages display in a container

--- app/Model/Order.php
+++ app/Model/Order.php
@@ -10,35 +10,36 @@ class Order extends AppModel {
 		$cart = CakeSession::read('Cart');
 		$inventory = ClassRegistry::init('InventoryItem');
 		$inventory->recursive = -1;
-		$inventoryItems = $inventory->find('all');
-		$data['InventoryItem'] = Hash::combine($inventoryItems, '{n}.InventoryItem.id', '{n}.InventoryItem');
+		$inventoryItems = Hash::combine($inventory->find('all'), '{n}.InventoryItem.id', '{n}.InventoryItem');
 		
 		$items = array();
-		foreach ($data['InventoryItem'] as $item) {
+		foreach ($inventoryItems as $item) {
 			$tee = $item['tee_id'];
 			$size = $item['size'];
 			$items[$tee][$size] = $item;
 		}
 		
+		$updatedItems = array();
+		
 		foreach ($cart as $id => $tee) {
 			foreach ($tee['sizes'] as $size => $item) {
 				if($items[$id][$size]['amount'] - $item['amount'] < 0){
-					$errorMessage = "Tyvärr finns det bara ".$items[$id][$size]['amount']." st. kvar i lager av t-shirt <em>".$tee['Tee']['name']."</em> i storlek ".$size.". Var god ändra din beställning.";
+					$this->lastErrorMessage = "Tyvärr finns det bara ".$items[$id][$size]['amount']." st. kvar i lager av t-shirt <em>".$tee['Tee']['name']."</em> i storlek ".$size.". Var god ändra din beställning.";
 					return false;
+				} else {
+					$items[$id][$size]['amount'] -= $item['amount'];
+					$updatedItems[] = $items[$id][$size];
 				}
-				
-				$data['InventoryItem'][$id]['amount'] -= $item['amount'];
 			}
 		}
 		
-		foreach ($cart as $id => $tee):
-			foreach ($tee['sizes'] as $size => $item):
-				$inventory->save($data['InventoryItem'][$id]);
-			endforeach;
-		endforeach;
-
-		$this->data['Order']['status'] = 'Bekräftad';
-		return true;
+		if ($inventory->saveAll($updatedItems)) {
+			$this->data['Order']['status'] = 'Bekräftad';
+			return true;
+		}
+		
+		$this->lastErrorMessage = "Okänt fel vid uppdatering av lagerstatus.";
+		return false;
 	}
 }
 ?>
