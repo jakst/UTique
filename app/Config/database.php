c9fccfadd583bd166c5a394bf5c5e9453de50777
Ylva Johansson <ylvjo680@student.liu.se>
2014-04-18 16:57:33 +0800

    ny loggo igen

--- app/Config/database.php
+++ app/Config/database.php
@@ -66,7 +66,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => '',
+		'password' => 'mysql',
 		'database' => 'utique',
 		// 'host' => 'sql3.freesqldatabase.com',
 		// 'login' => 'sql330561',
