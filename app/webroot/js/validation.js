2afb03dc368cb5ffcaff0b35eccd006c2fcd7165
Jakob Ståhl <jakob.stahl91@gmail.com>
2014-04-18 11:57:02 +0800

    More general validation js

--- app/webroot/js/validation.js
+++ app/webroot/js/validation.js
@@ -1,165 +1,22 @@
 $(document).ready(function(){
-	$('#name').blur(function(){
+	$('form').find('input').blur(function(){
+		var id = $(this).attr('id');
 		$.post(
 			'validate_form',
-			{field: $('#name').attr('id'), value: $('#name').val()},
-			handleNameValidation
+			{field: id, value: $(this).val()},
+			function(data) {handleValidation(id, data);}
 		);
 	});
-	
-	function handleNameValidation(error){
-		if (error.length > 0) {
-			if ($('#name-notEmpty').length == 0){
-				$('#name').after('<div id ="name-notEmpty" class="error-message">' + error + "</div>")
-			}
-		} else{
-			$('#name-notEmpty').remove();
-		}
-	}
-	
-	$('#email').blur(function(){
-		$.post(
-			'validate_form',
-			{field: $('#email').attr('id'), value: $('#email').val()},
-			handleEmailValidation
-		);
-	});
-	
-	function handleEmailValidation(error){
-		if (error.length > 0) {
-			if ($('#email-notEmpty').length == 0){
-				$('#email').after('<div id ="email-notEmpty" class="error-message">' + error + "</div>")
-			}
-		} else{
-			$('#email-notEmpty').remove();
-		}
-	}
-	
-	$('#address').blur(function(){
-		$.post(
-			'validate_form',
-			{field: $('#address').attr('id'), value: $('#address').val()},
-			handleAddressValidation
-		);
-	});
-	
-	function handleAddressValidation(error){
-		if (error.length > 0) {
-			if ($('#address-notEmpty').length == 0){
-				$('#address').after('<div id ="address-notEmpty" class="error-message">' + error + "</div>")
-			}
-		} else{
-			$('#address-notEmpty').remove();
-		}
-	}
-	
-	$('#zipcode').blur(function(){
-		$.post(
-			'validate_form',
-			{field: $('#zipcode').attr('id'), value: $('#zipcode').val()},
-			handleZipcodeValidation
-		);
-	});
-	
-	function handleZipcodeValidation(error){
-		if (error.length > 0) {
-			if ($('#zipcode-notEmpty').length == 0){
-				$('#zipcode').after('<div id ="zipcode-notEmpty" class="error-message">' + error + "</div>")
-			}
-		} else{
-			$('#zipcode-notEmpty').remove();
-		}
-	}
-	
-	$('#city').blur(function(){
-		$.post(
-			'validate_form',
-			{field: $('#city').attr('id'), value: $('#city').val()},
-			handleCityValidation
-		);
-	});
-	
-	function handleCityValidation(error){
-		if (error.length > 0) {
-			if ($('#city-notEmpty').length == 0){
-				$('#city').after('<div id ="city-notEmpty" class="error-message">' + error + "</div>")
-			}
-		} else{
-			$('#city-notEmpty').remove();
-		}
-	}
-	
-	$('#country').blur(function(){
-		$.post(
-			'validate_form',
-			{field: $('#country').attr('id'), value: $('#country').val()},
-			handleCountryValidation
-		);
-	});
-	
-	function handleCountryValidation(error){
-		if (error.length > 0) {
-			if ($('#country-notEmpty').length == 0){
-				$('#country').after('<div id ="country-notEmpty" class="error-message">' + error + "</div>")
-			}
-		} else{
-			$('#country-notEmpty').remove();
-		}
-	}
-	
-	$('#card_number').blur(function(){
-		$.post(
-			'validate_form',
-			{field: $('#card_number').attr('id'), value: $('#card_number').val()},
-			handleCardnumberValidation
-		);
-	});
-
-	$('#username').blur(function(){
-		alert ('Hej');
-		$.post(
-			'validate_user',
-			{field: $('#username').attr('id'), value: $('#username').val()},
-			handleUsernameValidation
-		);
-	});
-	
-	function handleUsernameValidation(error){
-		if (error.length > 0) {
-			if ($('#username-notEmpty').length == 0){
-				$('#username').after('<div id ="username-notEmpty" class="error-message">' + error + "</div>")
-			}
-		} else{
-			$('#username-notEmpty').remove();
-		}
-	}
-
-	
-	function handleCardnumberValidation(error){
-		if (error.length > 0) {
-			if ($('#card_number-notEmpty').length == 0){
-				$('#card_number').after('<div id ="card_number-notEmpty" class="error-message">' + error + "</div>")
-			}
-		} else{
-			$('#card_number-notEmpty').remove();
-		}
-	}
-	
-		$('#expiry_date').blur(function(){
-		$.post(
-			'validate_form',
-			{field: $('#expiry_date').attr('id'), value: $('#expiry_date').val()},
-			handleExpirydateValidation
-		);
-	});
-	
-	function handleExpirydateValidation(error){
-		if (error.length > 0) {
-			if ($('#expiry_date-notEmpty').length == 0){
-				$('#expiry_date').after('<div id ="expiry_date-notEmpty" class="error-message">' + error + "</div>")
-			}
-		} else{
-			$('#expiry_date-notEmpty').remove();
-		}
-	}
 });
+
+function handleValidation(id, error) {	
+	if (error.length > 0) {
+		if ($('#'+id+'-error').length == 0){
+			$('#'+id).parent().addClass('has-error');
+			$('#'+id).after('<div id ="'+id+'-error" class="error-message">' + error + "</div>")
+		}
+	} else {
+		$('#'+id).parent().removeClass('has-error');
+		$('#'+id+'-error').remove();
+	}
+}
