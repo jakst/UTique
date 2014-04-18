54f2351f62b747a595189da098ff654b93b813d4
Ylva Johansson <ylvjo680@student.liu.se>
2014-04-18 16:17:07 +0800

    ..

---
+++ app/webroot/js/5dfae6ce8dd4e667511c67e574a1762f.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {$("#link-582406526").bind("click", function (event) {$.ajax({url:"\/utique\/carts\/update_cart_item\/20\/XS\/6"});
+return false;});
+$("#link-366311371").bind("click", function (event) {$.ajax({url:"\/utique\/carts\/update_cart_item\/20\/XS\/8"});
+return false;});});
