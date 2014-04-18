5ea625e59a74bdf3d02d840bc0a4a325a407181a
sofienilsson90 <sofienilsson__@hotmail.com>
2014-04-18 09:25:33 +0200

    poop

---
+++ app/webroot/js/a8b594bccdfaed71811fabfd426496fd.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {$("#link-582406526").bind("click", function (event) {$.ajax({url:"\/utique\/carts\/update_cart_item\/14\/XS\/0"});
+return false;});
+$("#link-366311371").bind("click", function (event) {$.ajax({url:"\/utique\/carts\/update_cart_item\/14\/XS\/2"});
+return false;});});
---
+++ app/webroot/js/c6d8c4fb5304a44f15f6a845fee63ff5.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {$("#link-582406526").bind("click", function (event) {$.ajax({url:"\/utique\/carts\/update_cart_item\/20\/XS\/8"});
+return false;});
+$("#link-366311371").bind("click", function (event) {$.ajax({url:"\/utique\/carts\/update_cart_item\/20\/XS\/10"});
+return false;});});
